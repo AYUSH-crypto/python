e1509aa6e8ff9cad13bdeb83c0016e19c5c805ea
AYUSH-crypto <2024d2r017@mietjammu.in>
2025-12-04 01:22:39 +0530

    Implement loop with break and continue examples
    Add loop with break and continue statements

---
+++ 9.py
@@ -0,0 +1,7 @@
+for i in range(1, 10): 
+    if i == 5: 
+        break                   # exits loop when i is 5 
+    if i == 3: 
+        continue                # skips number 3 
+    pass                        # does nothing 
+    print(i)
