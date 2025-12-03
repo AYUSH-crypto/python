d302f68b90931e3ff1fc7583d08daefc86e4c7a8
AYUSH-crypto <2024d2r017@mietjammu.in>
2025-12-04 01:28:58 +0530

    Add list operations and print statements in 18.py

---
+++ 18.py
@@ -0,0 +1,9 @@
+lst = [5, 3, 9, 1] 
+print("Length:", len(lst)) 
+print("Is 3 in list?", 3 in lst) 
+lst.sort() 
+print("Sorted list:", lst) 
+lst.reverse() 
+print("Reversed list:", lst) 
+lst_copy = lst.copy() 
+print("Copied list:", lst_copy) 
