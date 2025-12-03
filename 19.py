21252aadf0b1d0b1bd8410f3ad8abb1b307a6d70
AYUSH-crypto <2024d2r017@mietjammu.in>
2025-12-04 01:29:23 +0530

    Implement list operations in 19.py

---
+++ 19.py
@@ -0,0 +1,8 @@
+lst = [1, 2, 2, 3] 
+print("Count of 2:", lst.count(2)) 
+print("Index of 3:", lst.index(3)) 
+lst.extend([4, 5]) 
+print("After extending:", lst) 
+print("Popped element:", lst.pop()) 
+lst.clear() 
+print("Cleared list:", lst)
